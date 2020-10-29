d4cec4f92df7d1b760bba374d304a40afe978487
popchim <gooooogle1503@gmai.com>
2020-10-29 07:52:46 +0700

    Adding item01 1.  Load an image by reading it from a file

--- lab01.cpp
+++ lab01.cpp
@@ -1,10 +1,18 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "iostream"
+#include <string>
+using namespace std;
 using namespace cv;
 int main(){
-  Mat image = imread("lena.png", IMREAD_UNCHANGED);
-  namedWindow("Lena");
-  imshow("Lena", image);
+  string item01 = "1.  Load an image by reading it from a file";
+  cout << item01 << endl;
+  string imagePath;
+  cout << "Enter the image path: ";
+  cin >> imagePath;
+  Mat image = imread(imagePath, IMREAD_UNCHANGED);
+  namedWindow(item01);
+  imshow(item01, image);
   waitKey(0);
   return 0;
 }
