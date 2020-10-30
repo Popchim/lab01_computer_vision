5468f85661c822e82bfff61d47099a6bf2bab606
popchim <gooooogle1503@gmai.com>
2020-10-30 23:27:56 +0700

    Added item04

--- lab01.cpp
+++ lab01.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "iostream"
 #include <string>
+#include <math.h>
 using namespace std;
 using namespace cv;
 void onTrackbar(int pos, void *dat){
@@ -12,29 +13,28 @@ void onTrackbar(int pos, void *dat){
   for( int y = 0; y < image.rows; y++ ) {
     for( int x = 0; x < image.cols; x++ ) {
        for( int c = 0; c < image.channels(); c++ ) {
-          changingImage.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[c] + pos - 255);
+          changingImage.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[c] * pow(1.016, pos - 255));
        }
     }
   }
   imshow(windowName, changingImage);
 }
 int main(){
-  string item03 = "3.  Change the brightness of the color image.  Add a trackbar in the output window tomodify the brightness parameter";
-  cout << item03 << endl;
+  string item04 = "4.  Change the contrast of the color image.  Add a trackbar in the output window tomodify the contrast parameter";
+  cout << item04 << endl;
   string imagePath;
   cout << "Enter the image path: ";
   cin >> imagePath;
   Mat image = imread(imagePath, IMREAD_UNCHANGED);
   Mat changingImage(image.rows, image.cols, CV_8UC3);
-  string windowName = "3.  Change the brightness of the color image.  Add a trackbar in the output window tomodify the brightness parameter";
   void *data[3];
   data[0] = &image;
   data[1] = &changingImage;
-  data[2] = &windowName;
-  namedWindow(windowName);
-  imshow(windowName, image);
-  int brightnessValue = 255;
-  createTrackbar("Brightness Trackbar", windowName, &brightnessValue, 511, onTrackbar, data);
+  data[2] = &item04;
+  namedWindow(item04);
+  imshow(item04, image);
+  int contrastValue = 255;
+  createTrackbar("Contrast Trackbar", item04, &contrastValue, 511, onTrackbar, data);
   waitKey(0);
   return 0;
 }
