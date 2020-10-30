b9d60886e5640f88b1ba6c48770ef381fa3320e2
popchim <gooooogle1503@gmai.com>
2020-10-30 21:49:09 +0700

    Add item02

--- lab01.cpp
+++ lab01.cpp
@@ -5,14 +5,25 @@
 using namespace std;
 using namespace cv;
 int main(){
-  string item01 = "1.  Load an image by reading it from a file";
-  cout << item01 << endl;
+  string item02 = "2.  Convert a color image into a grayscale image.  The command line is";
+  cout << item02 << endl;
   string imagePath;
   cout << "Enter the image path: ";
   cin >> imagePath;
-  Mat image = imread(imagePath, IMREAD_UNCHANGED);
-  namedWindow(item01);
-  imshow(item01, image);
+  size_t imagePathLength = imagePath.length();
+  string imageName = imagePath.substr(0, imagePathLength - 4);
+  string saveName = imageName.append("_GRAY.png");
+  Mat imageBGR = imread(imagePath, IMREAD_UNCHANGED);
+  Mat imageGRAY;
+  cvtColor(imageBGR, imageGRAY, COLOR_BGR2GRAY);
+  string windowBGR = "Your original image (BGR)";
+  string windowGRAY = "The grayscale version";
+  namedWindow(windowBGR);
+  imshow(windowBGR, imageBGR);
   waitKey(0);
+  namedWindow(windowGRAY);
+  imshow(windowGRAY, imageGRAY);
+  waitKey(0);
+  imwrite(saveName, imageGRAY);
   return 0;
 }
