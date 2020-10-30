e6fc647461bdffcfb31e52eedf1e625b9af1a306
popchim <gooooogle1503@gmai.com>
2020-10-30 23:05:47 +0700

    Add item03

--- lab01.cpp
+++ lab01.cpp
@@ -4,26 +4,37 @@
 #include <string>
 using namespace std;
 using namespace cv;
+void onTrackbar(int pos, void *dat){
+  void **data = static_cast<void**>(dat);
+  Mat& image = *(static_cast<Mat*>(data[0]));
+  Mat& changingImage = *(static_cast<Mat*>(data[1]));
+  string& windowName = *(static_cast<string*>(data[2]));
+  for( int y = 0; y < image.rows; y++ ) {
+    for( int x = 0; x < image.cols; x++ ) {
+       for( int c = 0; c < image.channels(); c++ ) {
+          changingImage.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(image.at<Vec3b>(y,x)[c] + pos - 255);
+       }
+    }
+  }
+  imshow(windowName, changingImage);
+}
 int main(){
-  string item02 = "2.  Convert a color image into a grayscale image.  The command line is";
-  cout << item02 << endl;
+  string item03 = "3.  Change the brightness of the color image.  Add a trackbar in the output window tomodify the brightness parameter";
+  cout << item03 << endl;
   string imagePath;
   cout << "Enter the image path: ";
   cin >> imagePath;
-  size_t imagePathLength = imagePath.length();
-  string imageName = imagePath.substr(0, imagePathLength - 4);
-  string saveName = imageName.append("_GRAY.png");
-  Mat imageBGR = imread(imagePath, IMREAD_UNCHANGED);
-  Mat imageGRAY;
-  cvtColor(imageBGR, imageGRAY, COLOR_BGR2GRAY);
-  string windowBGR = "Your original image (BGR)";
-  string windowGRAY = "The grayscale version";
-  namedWindow(windowBGR);
-  imshow(windowBGR, imageBGR);
+  Mat image = imread(imagePath, IMREAD_UNCHANGED);
+  Mat changingImage(image.rows, image.cols, CV_8UC3);
+  string windowName = "3.  Change the brightness of the color image.  Add a trackbar in the output window tomodify the brightness parameter";
+  void *data[3];
+  data[0] = &image;
+  data[1] = &changingImage;
+  data[2] = &windowName;
+  namedWindow(windowName);
+  imshow(windowName, image);
+  int brightnessValue = 255;
+  createTrackbar("Brightness Trackbar", windowName, &brightnessValue, 511, onTrackbar, data);
   waitKey(0);
-  namedWindow(windowGRAY);
-  imshow(windowGRAY, imageGRAY);
-  waitKey(0);
-  imwrite(saveName, imageGRAY);
   return 0;
 }
