b123bb2d2e0cbdcdf83a9d6330f0702fc387a352
popchim <gooooogle1503@gmai.com>
2020-10-31 09:54:16 +0700

    Added item06

--- lab01.cpp
+++ lab01.cpp
@@ -8,31 +8,31 @@
 using namespace std;
 using namespace cv;
 void onTrackbar(int pos, void *dat){
-  if (pos != 0) {
   void **data = static_cast<void**>(dat);
-    Mat& image = *(static_cast<Mat*>(data[0]));
-    Mat& changingImage = *(static_cast<Mat*>(data[1]));
-    string& windowName = *(static_cast<string*>(data[2]));
-    blur(image, changingImage, Size(pos, pos));
-    imshow(windowName, changingImage);
-  }
+  Mat& image = *(static_cast<Mat*>(data[0]));
+  Mat& changingImage = *(static_cast<Mat*>(data[1]));
+  string& windowName = *(static_cast<string*>(data[2]));
+  GaussianBlur(image, changingImage, Size(pos*2 + 1, pos*2 + 1), 1);
+  imshow(windowName, changingImage);
 }
 int main(){
-  string item05 = "Filter a color image/ a grayscale image usingaverage filter operator. Add a trackbarin the output window to modify the kernel size parameter";
-  cout << item05 << endl;
+  string item06 = "Filter a color image/ a grayscale image using Gaussian filter operator. Add a trackbarin the output window to modify the kernel size parameter";
+  cout << item06 << endl;
   string imagePath;
   cout << "Enter the image path: ";
   cin >> imagePath;
   Mat image = imread(imagePath, IMREAD_UNCHANGED);
-  Mat changingImage(image);
+  Mat changingImage;
   void *data[3];
   data[0] = &image;
   data[1] = &changingImage;
-  data[2] = &item05;
-  namedWindow(item05);
-  imshow(item05, image);
-  int kernelSize = 1;
-  createTrackbar("Kernel Size Trackbar", item05, &kernelSize, max(image.rows, image.cols), onTrackbar, data);
+  data[2] = &item06;
+  namedWindow("original");
+  imshow("original", image);
+  namedWindow(item06);
+  imshow(item06, image);
+  int kernelSize = 0;
+  createTrackbar("Kernel Size Trackbar", item06, &kernelSize, int(max(image.rows, image.cols)/2-1), onTrackbar, data);
   waitKey(0);
   return 0;
 }
